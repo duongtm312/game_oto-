331c593a905f2c9d01acbaf54ecf98d9ff99d01c
duongtm312 <103014462+duongtm312@users.noreply.github.com>
2022-04-25 09:03:32 +0700

    up

--- script.js
+++ script.js
@@ -1,93 +1,87 @@
-function myCanvas() {
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-    return ctx;
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let background = new Image();
+let carimg= new Image();
+let coinimg=new Image();
+let x=0;
+let y=-5;
+let img=1
+let fame=60
+let count=0;
+let check=true;
+background.src="hinhnen.jpg";
+coinimg.src="coin.png";
+carimg.src;
+
+let car={
+    x:400,
+    y:200
 }
-
-function drawCar(i, x, y) {
-    base_image = new Image();
-    base_image.src = `oto${i}.png`;
-    base_image.onload = function () {
-        ctx.drawImage(base_image, x, y, 45, 45);
-    }
+let coin= {
+    x: Math.floor(Math.random()*100 )*5,
+    y: Math.floor(Math.random()*100 )*5,
 }
+console.log(coinimg);
+console.log(coin)
 
-function clearCanvas(x, y) {
-    ctx.fillStyle = "#ffffff"
-    ctx.fillRect(x, y, 600, 600);
-
-}
-
-let ctx = myCanvas();
-let x = 0;
-let y = 0;
-let i = 1;
-let a = 250;
-let b = 150;
-let count = 0;
-drawCar(i, x, y);
-
-function moveSelection(evt) {
-    switch (evt.keyCode) {
-        case 37:
-            goCar(-20, 0, 4)
-            break;
-        case 39:
-            goCar(20, 0, 2);
-            break;
-        case 38:
-            goCar(0, -20, 1);
-            break
-        case 40:
-            goCar(0, 20, 3);
-            break
-    }
-}
-
-function goCar(t, p, o) {
-    clearCanvas(x, y)
-    x = x + t;
-    y = y + p
-    i = o;
-    drawCar(i, x, y);
-    drawCoin()
-    checkLose()
-    checkpoint()
-}
-
-function checkLose() {
-    if (x > 560 || x < 0 || y > 560 || y < 0) {
+function run(){
+    ctx.drawImage(background,0,0)
+    carimg.src=`oto${img}.png`
+    ctx.drawImage(carimg,car.x,car.y,30,30)
+    ctx.drawImage(coinimg,coin.x,coin.y,30,30)
+    car.x+=x;
+    car.y+=y;
+    if (car.x + 30> 800 || car.x < 0 || car.y+30 > 400 || car.y < 0) {
         count = 0;
-        x = 200, y = 200
+        car.y = 200, car.y = 200
         document.getElementById("point").innerHTML = "Điểm :" + count
-        clearCanvas()
-        drawCar()
+        return;
     }
-}
-
-function checkpoint() {
-    if (a > x && a < x + 45 && b < y + 45 && b > y) {
-        a = radom()
-        b = radom()
-        drawCoin(a, b)
-        drawCar(i, x, y)
+    if (car.x==coin.x+15&&car.y==coin.y){
         count++
         document.getElementById("point").innerHTML = "Điểm :" + count
     }
+    // requestAnimationFrame(run)
+    setTimeout(run,fame)
+
 }
 
-function radom() {
-    let r = Math.random() * 400
-    return r
+run()
+function moveSelection(evt) {
+    switch (evt.keyCode) {
+        case 37:
+            x=-5
+            y=0
+            img=4
+            break;
+        case 39:
+            x=5
+            y=0
+            img=2
+            break;
+        case 38:
+            x=0
+            y=-5
+            img=1
+            break
+        case 40:
+            x=0
+            y=5
+            img=3
+            break
+        case 17:
+            if (check){
+                fame=10;
+                check=false;
+            }else {
+                fame=60;
+                check=true;
+            }
+    }
 }
 
-function drawCoin() {
-    ctx.fillStyle = "#ff0066"
-    ctx.fillRect(a, b, 20, 20);
-}
+
 
 function docReady() {
     window.addEventListener('keydown', moveSelection);
 }
-
-drawCoin()
